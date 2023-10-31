7ec39bc13a3120c22ee1d1c017fad1264a57e05c
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2023-10-31 18:48:40 +0000

    Fixed issue #2 and #3

--- js/main.js
+++ js/main.js
@@ -2,171 +2,158 @@ import * as imageProcessing from './imageProcessing.js';
 import { createZipFile, generateZipURL } from './fileIO.js';
 import * as utils from './utils.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Get references to HTML elements
+document.addEventListener('DOMContentLoaded', initializePage);
+
+function initializePage() {
     const uploadForm = document.getElementById('upload-form');
     const fileInput = document.getElementById('file-upload');
     const gridSizeInput = document.getElementById('grid');
     const prefixInput = document.getElementById('prefix');
     const downloadButton = document.getElementById('download');
     const commandTextArea = document.getElementById('commandTextArea');
-
+    
     let imageUrl = null;
+    let zipUrl = null;
+    
+    gridSizeInput.addEventListener('input', updateGridLines);
+    fileInput.addEventListener('change', handleFileChange);
+    uploadForm.addEventListener('submit', handleFormSubmit);
+    downloadButton.addEventListener('click', downloadZip);
 
-    gridSizeInput.addEventListener('input', () => {
+    downloadButton.disabled = true;
+
+    function updateGridLines() {
+        // console.log("updateGridLines");
         const gridSize = parseInt(gridSizeInput.value);
         if (imageUrl) {
-            // Call the drawGridLines function to update the grid overlay
+            clearCanvas(); // Clear the canvas
+            renderPreview(imageUrl);
             drawGridLines(imageUrl, gridSize);
         }
-    });
+    }
 
-     // Event listener for file input change event
-    fileInput.addEventListener('change', (event) => {
+    function handleFileChange(event) {
+        // console.log("handleFileChange");
         const selectedFile = event.target.files[0];
         if (selectedFile) {
-             // Check if the selected file is a GIF
             if (selectedFile.type === 'image/gif') {
                 alert('GIF files are not supported currently. Please select a different image format.');
-                return; 
+                return;
             }
             imageUrl = URL.createObjectURL(selectedFile);
             const fileName = selectedFile.name;
             document.getElementById('file-name').textContent = fileName;
-
-            renderPreview(imageUrl); // Display a preview of the selected image
+            clearCanvas(); // Clear the canvas
+            renderPreview(imageUrl);
+           
         }
-    });
+    }
 
-    downloadButton.disabled = true;
-    let zipUrl = null;
-
-    uploadForm.addEventListener('submit', async (event) => {
+    function handleFormSubmit(event) {
+        // console.log("handleFormSubmit");
         event.preventDefault();
-
         if (!imageUrl) {
             alert('Please select an image to split.');
             return;
         }
-
         const gridSize = parseInt(gridSizeInput.value);
         const prefix = prefixInput.value;
-
-        // Disable the download button while preparing the image
         downloadButton.disabled = true;
 
         const fileName = utils.getFileNameFromItem(imageUrl, prefix);
-        const { images, textCommand } = await imageProcessing.createSmallerImages(imageUrl, gridSize, fileName);
-        const zip = createZipFile(fileName, images, textCommand, gridSize);
-        zipUrl = await generateZipURL(zip);
+        imageProcessing.createSmallerImages(imageUrl, gridSize, fileName)
+            .then(({ images, textCommand }) => {
+                const zip = createZipFile(fileName, images, textCommand, gridSize);
+                return generateZipURL(zip);
+            })
+            .then((url) => {
+                zipUrl = url;
+                downloadButton.disabled = false;
+                clearCanvas(); // Clear the canvas
+                renderPreview(imageUrl);
+                drawGridLines(imageUrl, gridSize);
+                const slackbotCommand = utils.generateSlackbotCommand(gridSize, prefix);
+                commandTextArea.value = slackbotCommand;
+            });
+    }
 
-        // Enable the download button after preparing the ZIP
-        downloadButton.disabled = false;
-
-         // Call the drawGridLines function to show red grid lines
-        drawGridLines(imageUrl, gridSize);
-
-        // Generate and set the Slackbot command
-        const slackbotCommand = utils.generateSlackbotCommand(gridSize, prefix);
-        commandTextArea.value = slackbotCommand;
-
-         // Generate and set the Slackbot command with line breaks and without spaces
-        const slackbotCommandWithLineBreaksAndNoSpaces = slackbotCommand
-            .replace(/\\n/g, '\n')
-            .replace(/ /g, '');
-        commandJustCopyTextArea.value = slackbotCommandWithLineBreaksAndNoSpaces;
-    });
-
-    // Attach a click event listener to the download button
-    downloadButton.addEventListener('click', () => {
+    function downloadZip() {
         if (zipUrl) {
-            // Create a hidden download link and trigger a click event to start the download
             const downloadLink = document.createElement('a');
             downloadLink.href = zipUrl;
             downloadLink.download = `${prefixInput.value || 'bigmoji'}.zip`;
             downloadLink.style.display = 'none';
-
-            // Append the link to the body and trigger the click event to start the download
             document.body.appendChild(downloadLink);
             downloadLink.click();
             document.body.removeChild(downloadLink);
         }
-    });
+    }
 
-    // Function to render a preview of the selected image
     function renderPreview(imageUrl) {
+        // console.log("renderPreview");
         const previewDiv = document.getElementById('preview-div');
         previewDiv.innerHTML = '';
         const img = document.createElement('img');
         img.src = imageUrl;
+        previewDiv.style.position = 'relative';
+        img.style.width = '100%'; // Set the canvas width to 100%
+        img.style.height = 'auto'; // Set the canvas height to auto
         previewDiv.appendChild(img);
     }
-    
-    // Function to draw red grid lines over the image
-    // Function to draw red grid lines over the image
+
     function drawGridLines(imageUrl, gridSize) {
         const previewDiv = document.getElementById('preview-div');
-
-        // Create a container div for the image and grid overlay
         const containerDiv = document.createElement('div');
         containerDiv.style.position = 'relative';
 
-        // Create a fixed-size canvas element
         const canvas = document.createElement('canvas');
-        canvas.width = previewDiv.clientWidth;
-        canvas.height = previewDiv.clientHeight;
+        canvas.style.width = '100%'; // Set the canvas width to 100%
+        canvas.style.height = 'auto'; // Set the canvas height to auto
+
         const context = canvas.getContext('2d');
 
-        // Create the image element
         const img = new Image();
-        // Set the image's src to reload it
         img.src = imageUrl;
 
-        // Wait for the image to load
         img.onload = () => {
-            // Calculate the dimensions of each grid cell
-            const cellWidth = canvas.width / gridSize;
-            const cellHeight = canvas.height / gridSize;
+            const aspectRatio = img.width / img.height;
+            canvas.height = canvas.width / aspectRatio; // Set the canvas height based on aspect ratio
 
-            // Set the stroke color to red
-            context.strokeStyle = 'red';
-
-            // Draw the image on the canvas, scaling it to fit the canvas dimensions
             context.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-            // Draw vertical grid lines
+            context.strokeStyle = 'red';
+            const cellSize = canvas.width / gridSize;
+
             for (let i = 1; i < gridSize; i++) {
-                const x = i * cellWidth;
+                const position = i * cellSize;
                 context.beginPath();
-                context.moveTo(x, 0);
-                context.lineTo(x, canvas.height);
+
+                context.moveTo(position, 0);
+                context.lineTo(position, canvas.height);
+                context.moveTo(0, position);
+                context.lineTo(canvas.width, position);
+
                 context.stroke();
             }
 
-            // Draw horizontal grid lines
-            for (let i = 1; i < gridSize; i++) {
-                const y = i * cellHeight;
-                context.beginPath();
-                context.moveTo(0, y);
-                context.lineTo(canvas.width, y);
-                context.stroke();
-            }
-
-            // Append the canvas to the container
             containerDiv.appendChild(canvas);
             previewDiv.innerHTML = ''; // Clear previous content
             previewDiv.appendChild(containerDiv);
         };
     }
+    
+    window.addEventListener('beforeunload', clearInputFields);
 
-   
-
-    // Add an event listener to clear input fields on page reload
-    window.addEventListener('beforeunload', () => {
+    function clearInputFields() {
         fileInput.value = '';
         gridSizeInput.value = '2';
         prefixInput.value = '';
         commandTextArea.value = '';
-        commandJustCopyTextArea.value = '';
-    });
-});
+    }
+
+    function clearCanvas() {
+        const previewDiv = document.getElementById('preview-div');
+        previewDiv.innerHTML = '';
+    }
+}
+
